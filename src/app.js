e20dae244882c13937dc11b20151adc98d5884a5
unknown <ishita.agarwal@briskminds.com>
2023-06-05 13:25:56 +0530

    commitsecond

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 require('./db/conn')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5200;
 const app = express();
 const studentRouter = require('./router/student')
 
 app.use(express.json())
 app.use(studentRouter)
+
+app.use((req,res,next) => {
+    console.log("in the middleware");
+    res.send('<h1>add product</h1>');
+});
 
 app.listen(port, () => {
     console.log('Connection successfully..........');
